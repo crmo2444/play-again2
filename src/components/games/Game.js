74729d03b91ae8de7cbc1a877ce1bd5208620ebc
Claire Morgan-Sanders <crmo2444@gmail.com>
2022-06-14 14:09:27 -0500

    user can't add game to wishlist/library if it's already there

--- src/components/games/Game.js
+++ src/components/games/Game.js
@@ -3,11 +3,23 @@ import { Link, useNavigate } from "react-router-dom"
 
 export const Game = ({gameObject}) => {
     const [feedback, setFeedback] = useState("")
+    const [libraryAndWishlist, setLibraryAndWishlist] = useState([])
 
     let navigate = useNavigate()
 
     const localUser = localStorage.getItem("current_user")
     const localUserObject = JSON.parse(localUser)
+
+    useEffect(
+        () => {
+            fetch(`http://localhost:8088/libraryAndWishlist`)
+                .then(response=>response.json())
+                .then((data) => {
+                    setLibraryAndWishlist(data)
+                })
+        },
+        []
+    )
 
     useEffect(() => {
         if (feedback !== "") {
@@ -16,27 +28,52 @@ export const Game = ({gameObject}) => {
         }
     }, [feedback])
 
-    const addToLibrary = () => {
-        const newGame = {
-            userId: localUserObject.id,
-            gameId: gameObject.id,
-            library: true,
-            wishlist: false
-        }
+    const checkLibrary = () => {
 
-    // TODO: Perform the fetch() to POST the object to the API
-    fetch('http://localhost:8088/libraryAndWishlist', {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(newGame)
-    })
-        .then(response => response.json())
-        .then(() => {
-            setFeedback("Added to Library!")
+        let foundGame = libraryAndWishlist.find(game => game.gameId === gameObject.id && game.library === true)
+
+        if (typeof foundGame === 'undefined') {
+            addToLibrary()
+        }
+        else {
+            return window.alert(`${gameObject.name} already in library!`)
+        }
+        
+    }
+
+    const addToLibrary = () => {
+            const newGame = {
+                userId: localUserObject.id,
+                gameId: gameObject.id,
+                library: true,
+                wishlist: false
+            }
+    
+        // TODO: Perform the fetch() to POST the object to the API
+        fetch('http://localhost:8088/libraryAndWishlist', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(newGame)
         })
-}
+            .then(response => response.json())
+            .then(() => {
+                setFeedback("Added to Library!")
+            })
+    }
+
+    const checkWishlist = () => {
+
+        let foundGame = libraryAndWishlist.find(game => game.gameId === gameObject.id && game.wishlist === true)
+
+        if (typeof foundGame === 'undefined') {
+            addToWishlist()
+        }
+        else {
+            return window.alert(`${gameObject.name} already in wishlist!`)
+        }
+    }
 
     const addToWishlist = () => {
         const newGame = {
@@ -67,7 +104,7 @@ export const Game = ({gameObject}) => {
                 <h4>{gameObject.name}</h4>
                 {/* <img className="resultPictures" src={result?.short_screenshots[0]?.image}/> */}
                 <button onClick={() => navigate(`/game/${gameObject.id}`)}>Details</button>
-                <button onClick={(event) => addToLibrary(event)}>Add to Library</button>
-                <button onClick={(event) => addToWishlist(event)}>Add to Wishlist</button>
+                <button onClick={(event) => checkLibrary(event)}>Add to Library</button>
+                <button onClick={(event) => checkWishlist(event)}>Add to Wishlist</button>
             </section>
 }
